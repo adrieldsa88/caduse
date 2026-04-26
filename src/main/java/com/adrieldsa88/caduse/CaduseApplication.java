1ad150012c52517aacd92de2751cea1f26c01e01
Adriel Silva <adrieldsa88@gmail.com>
2026-04-26 16:44:06 -0300

    initial commit

---
+++ src/main/java/com/adrieldsa88/caduse/CaduseApplication.java
@@ -0,0 +1,13 @@
+package com.adrieldsa88.caduse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaduseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaduseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrieldsa88/caduse/CaduseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrieldsa88.caduse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaduseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
